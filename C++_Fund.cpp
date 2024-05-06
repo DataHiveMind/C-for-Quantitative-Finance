ba7d293b7ec2d88405b909d17313ed6f1c2491a1
DataHiveMind <77356196+DataHiveMind@users.noreply.github.com>
2024-05-06 18:30:39 -0400

    T1

---
+++ C++_Fund.cpp
@@ -0,0 +1,24 @@
+#include iostream;
+
+using namespace std;
+
+int main(){
+    cout << "hello world!";
+}
+
+int lesson_3(){
+    const int monthsInYear = 12;
+    double principal = 100000.0;
+    double interest = 0.05;
+    int yearOfLoan = 30;
+
+    double monthInterest = interest/monthsInYear;
+    long monthsOfLoan = yearsOfLoan * monthsInYear;
+
+    cout << principal <<" "<< interest <<" "<< yearOfLoan << " "<< monthInterest <<" "<< monthsOfLoan << endl;
+    return 0;
+}
+
+int lesson_4(){
+
+}
